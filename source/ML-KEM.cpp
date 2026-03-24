4b50a596db0987d9047e329594764c5bf8b432b5
JoseAyeras <jose_ayeras@yahoo.com>
2026-03-24 15:13:20 -0700

    keygen progress 0

--- source/ML-KEM.cpp
+++ source/ML-KEM.cpp
@@ -9,6 +9,7 @@ void KeyGen_internal(
         std::array<uint8_t, 800> & ek, //output: encapsulation key ek
         std::array<uint8_t, 1632> & dk  //output: decapsulation key dk
 ){
-    std::array<uint8_t, 800> ekpke, dkpke;
-    K_PKE::KeyGen(d, ekpke, dkpke);
+    std::array<uint8_t, 800> dkpke;
+    K_PKE::KeyGen(d, ek, dkpke); //intermediary ekpke unnecessary due to ek and ekpke being equal size
+
 }
