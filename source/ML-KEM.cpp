939c2dcee113463325d20e50647dfcbdc0fe153f
Joselito Ayeras <jose_ayeras@yahoo.com>
2026-03-26 19:34:38 -0700

    progress 2026 03 26

--- source/ML-KEM.cpp
+++ source/ML-KEM.cpp
@@ -1,5 +1,6 @@
 #include "ML-KEM.hpp"
 #include "K-PKE.hpp"
+#include <cstring>
 
 using namespace ML_KEM;
 
@@ -11,5 +12,16 @@ void KeyGen_internal(
 ){
     std::array<uint8_t, 800> dkpke;
     K_PKE::KeyGen(d, ek, dkpke); //intermediary ekpke unnecessary due to ek and ekpke being equal size
+    size_t offset = 0;
 
+    std::memcpy(dk.data() + offset, dkpke.data(), 800);
+    offset += 800;
+
+    std::memcpy(dk.data() + offset, ek.data(), 800);
+    offset += 800;
+
+    std::memcpy(dk.data() + offset, hash_ek.data(), 32);
+    offset += 32;
+
+    std::memcpy(dk.data() + offset, z.data(), 32);
 }
--- source/ML-KEM.hpp
+++ source/ML-KEM.hpp
@@ -13,4 +13,14 @@ namespace ML_KEM
         std::array<uint8_t, 800> & ek, //output: encapsulation key ek
         std::array<uint8_t, 1632> & dk  //output: decapsulation key dk
     );
+    void Encaps_internal();
+    void Decaps_internal();
+
+    void KeyGen(
+        std::array<uint8_t, 32> d, //input: random number d
+        std::array<uint8_t, 800> & ekpke, //output: encapsulation key ekpke
+        std::array<uint8_t, 800> & dkpke  //output: decapsulation key dkpke
+    );
+    void Encaps();
+    void Decaps();
 }
