b748a911eafff3f248b8c09972ecb151cd12c019
JoseAyeras <jose_ayeras@yahoo.com>
2026-05-06 10:33:12 -0700

    Update K-MATH.hpp
    Started K-MATH

--- source/K-MATH.hpp
+++ source/K-MATH.hpp
@@ -4,5 +4,14 @@
 #include <array>
 
 namespace K_MATH {
-    
+    void BitsToBytes();
+    void BytesToBits();
+    void ByteEncode();
+    void ByteDecode();
+    void SampleNTT();
+    void SamplePolyCBD();
+    void NTT();
+    void InvNTT();
+    void MultiplyNTTs();
+    void BaseCaseMultiply();
 }
